faa077e6cc0f139cd237b80575f714959403ad62
марк толстов <mrmarckis@gmail.com>
2024-06-09 15:41:07 +0700

    jumpToNextForm checkBox color but no mark

--- app/src/main/java/com/example/a9tolstov102/MainActivity.java
+++ app/src/main/java/com/example/a9tolstov102/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.a9tolstov102;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,12 +17,43 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+
+
+        Button btnReg = findViewById(R.id.btnSignUp);
+        CheckBox checkBox = findViewById(R.id.cbAgree);
+
+        View.OnClickListener MainApp = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (checkBox.isChecked()) {
+                    Intent intent = new Intent(MainActivity.this, activity_1.class);
+                    startActivity(intent);
+                }
+            }
+        };
+
+        btnReg.setOnClickListener(MainApp);
     }
+
+    /*fun onClickStepsActivity(view: View) {
+        val intent = Intent(this, StepsActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun onCheckboxClicked(view: View) {
+        // Получаем функцию перехода на другой экран:
+        val checkBox = view as CheckBox
+        val selection = findViewById<TextView>(R.id.selection)
+        // Получаем, отмечен ли данный флажок
+        if (checkBox.isChecked) {
+            selection.text = "Включено"
+            checkBox.text = "Выключить"
+        } else {
+            selection.text = "Выключено"
+            checkBox.text = "Включить"
+        }
+    }*/
+
 }
---
+++ app/src/main/java/com/example/a9tolstov102/activity_1.java
@@ -0,0 +1,18 @@
+package com.example.a9tolstov102;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class activity_1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_1);
+    }
+}
